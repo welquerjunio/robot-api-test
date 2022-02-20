6c60f8d8ad5cd71f9dd9e69818438a6f2e0da5c2
welquerjunio <welquerrodrigues.05@gmail.com>
2022-02-19 21:56:09 -0300

    subindo arquivos test api

---
+++ factories/Thanos.py
@@ -0,0 +1,11 @@
+from faker import Faker
+faker = Faker()
+
+def factory_thanos():
+    return{
+        "name": faker.name(),
+        "aliases": "Thanos",
+        "age": 3000,
+        "team": "Ordem Negra",
+        "active": False
+    }
